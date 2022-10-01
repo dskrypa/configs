d730bf0259923f00e93d7a8304df7076080e4430
dskrypa <dskrypa@gmail.com>
2022-10-01 18:17:14 -0400

    updated logging init

--- ipython_profile/startup/00_init_logger.py
+++ ipython_profile/startup/00_init_logger.py
@@ -6,6 +6,15 @@ import warnings
 log = logging.getLogger(__name__)
 warnings.filterwarnings('ignore', '.*found in sys.modules after import of package.*but prior to execution of.*', RuntimeWarning)
 
+SET_LEVELS = {
+    'asyncio': 50,
+    'parso.cache': 50,
+    'parso.python.diff': 50,
+    # 'urllib3.connectionpool': 50,
+    'numba': 50,
+    'paramiko.transport': 50,
+}
+
 try:
     from ds_tools.logging import init_logging
     from ds_tools.output import Printer; p = Printer('pseudo-json')
@@ -16,13 +25,15 @@ try:
         log_path=None,
         # log_path='/var/tmp/ipython3_interactive.log',
         entry_fmt='%(asctime)s %(levelname)s %(name)s %(lineno)d %(message)s',
-        # names=['ds_tools', 'requests_client', 'music_manager', '__main__', 'tz_aware_dt'],
+        # names=['ds_tools', 'requests_client', 'music_manager', '__main__', 'tz_aware_dt', 'wiki_nodes', 'music', 'lib', 'pypod', 'rpi_hvac'],
         names=None,
         millis=True,
-        set_levels={'asyncio': 50, 'parso.cache': 50},
+        set_levels=SET_LEVELS,
     )
 except ImportError:
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(lineno)d %(message)s')
+    for logger_name, level in SET_LEVELS.items():
+        logging.getLogger(logger_name).setLevel(level)
 
 
 class RequestFilter(logging.Filter):    # Make requests_client logged requests gray
