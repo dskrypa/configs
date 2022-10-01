680a5d23aaf369173194700a7b62267c05aeb26b
dskrypa <dskrypa@gmail.com>
2022-10-01 18:18:57 -0400

    updated ipython config

--- ipython_profile/ipython_config.py
+++ ipython_profile/ipython_config.py
@@ -2,13 +2,13 @@
 Location: ~/.ipython/profile_default/ipython_config.py
 
 For better pprint, ``pip install rich``
+
+To debug a silent crash likely due to infinite recursion, run with env var: PYTHONDEVMODE=1
 """
 
-# from io import StringIO
 from typing import TYPE_CHECKING
 
 from IPython.core.formatters import PlainTextFormatter, catch_format_error
-# from IPython.lib.pretty import RepresentationPrinter, Breakable
 from IPython.terminal.prompts import ClassicPrompts
 
 if TYPE_CHECKING:
@@ -35,49 +35,9 @@ else:
     console = Console(highlight=False)
 
     def _format_obj(self, obj):
+        # Note: This is failing for importlib.metadata.EntryPoint objects (infinite recursion)
+        # console.print(Pretty(obj, highlighter=highlighter, max_depth=30), soft_wrap=True)
         console.print(Pretty(obj, highlighter=highlighter), soft_wrap=True)
         return None  # Tells ipython that the object has already been printed
 
     PlainTextFormatter.__call__ = catch_format_error(_format_obj)
-
-
-# WIP: Patch for default repr pprinter to make output more symmetrical
-
-# class BetterPrinter(RepresentationPrinter):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(StringIO(), *args, **kwargs)
-#
-#     def begin_group(self, indent=0, open=''):
-#         print(f'begin_group: {indent=}, {open=}')
-#         indent += 3 if indent else 0
-#         indent_str = ' ' * (self.indentation + indent)
-#         open = f'{open}\n{indent_str}' if open else open
-#         print(f'  > begin_group: {indent=}, {open=}')
-#         super().begin_group(indent, open)
-#
-#     def end_group(self, dedent=0, close=''):
-#         print(f'end_group: {dedent=}, {close=}')
-#         dedent += 3 if dedent else 0
-#         # indent_str = ' ' * (self.indentation + dedent)
-#         close = f'\n{close}' if close else close
-#         print(f'  > end_group: {dedent=}, {close=}')
-#         super().end_group(dedent, close)
-#
-#     def pformat(self, obj):
-#         self.pretty(obj)
-#         self.flush()
-#         return self.output.getvalue()
-
-
-# def _format_obj(self, obj):
-#     if not self.pprint:
-#         return repr(obj)
-#     else:
-#         return BetterPrinter(
-#             self.verbose, self.max_width, self.newline, max_seq_length=self.max_seq_length,
-#             singleton_pprinters=self.singleton_printers, type_pprinters=self.type_printers,
-#             deferred_pprinters=self.deferred_printers,
-#         ).pformat(obj)
-
-
-# PlainTextFormatter.__call__ = catch_format_error(_format_obj)
